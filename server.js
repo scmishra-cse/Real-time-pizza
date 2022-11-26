4825eeeeb2acf9b9ef76e8dce46633b72c252171
Shubham Chnadra Mishra <phsmishra@gmail.com>
2022-11-26 08:13:42 +0530

    'initial'

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const expressLayouts = require('express-Ejs-layouts');
 
+// setting port number
 const PORT = process.env.PORT || 3000
 
+// Creating routes 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//set Template engine
+app.use(expressLayouts)
+app.set('views', path.join(__dirname, '/res/views'))
+app.set('view engine', 'ejs')
+
+//creating server
 app.listen(PORT, () => {
-    console.log('Hello on PORT 3000')
+    console.log(`Hello on PORT ${PORT}`)
 })
